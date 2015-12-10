aaa8fb818f3508fb76dd2cade2350f869a086cff
Neil Breeden III <neil@htmlbuffet.com>
2015-12-10 12:20:40 -0800

    Adding initial sound implementation.

---
+++ src/assets/js/soundManager.js
@@ -0,0 +1,36 @@
+var soundManager = (function() {
+    var popIndex = 0;
+    var lostIndex = 0;
+    var queueLength = 5;
+    var popAudio = [];
+    var lostAudio = [];
+
+    function initialize() {
+        for (var i=0; i<queueLength; i++) {
+            popAudio.push(new Audio('assets/audio/balloon-popping.wav'));
+            lostAudio.push(new Audio('assets/audio/balloon-water.wav'));
+        }
+    }
+
+    function playPop() {
+        popIndex++;
+        if (popIndex >= popAudio.length) {
+            popIndex = 0;
+        }
+        popAudio[popIndex].play();
+    }
+
+    function playLost() {
+        lostIndex++;
+        if (lostIndex >= lostAudio.length) {
+            lostIndex = 0;
+        }
+        lostAudio[lostIndex].play();
+    }
+
+    return {
+        initialize: initialize,
+        playPop: playPop,
+        playLost: playLost
+    }
+}());
